7e2e9d5944424bad64e4e3887b16014b1398e329
Saionaid <Saionaid@gmail.com>
2021-03-17 14:35:55 +0200

    model

--- Epic_Car_Store/carshop/models.py
+++ Epic_Car_Store/carshop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Car_1(models.Model):
+    name = models.CharField(max_length=60)
+    perishable = models.BooleanField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Car_2(models.Model):
+    name = models.CharField(max_length=60)
+    type = models.ForeignKey(Car_1, on_delete=models.DO_NOTHING, null=True, blank=True)
+    year_released = models.PositiveIntegerField(default=0)
+    description = models.TextField(null=True, blank=True)
+    in_stock = models.BooleanField(null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=0)
+    date_added = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name + '-' + str(self.id)
