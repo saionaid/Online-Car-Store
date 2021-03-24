9bddb4db4461b44b324ef379222f8e2af3ed548d
Saionaid <Saionaid@gmail.com>
2021-03-24 16:03:43 +0200

    update

--- Epic_Car_Store/Epic_Car_Store/urls.py
+++ Epic_Car_Store/Epic_Car_Store/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from carshop.views import homepage_view, free1, free2, free3, free4
+from carshop.views import homepage_view, cars, contact_form, login, free4
 
 urlpatterns = [
-    path('', homepage_view, name='Welcome'),
-    path('free1/', free1),
-    path('free2/', free2),
-    path('free3/', free3),
+    path('', homepage_view),
+    path('cars/', cars),
+    path('contact_form/', contact_form),
+    path('Login/', login),
     path('free4/', free4),
     path('admin/', admin.site.urls),
 ]
--- Epic_Car_Store/carshop/views.py
+++ Epic_Car_Store/carshop/views.py
@@ -6,14 +6,14 @@ from django.http import HttpResponse
 def homepage_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
-def free1(request, *args, **kwargs):
-    return render(request, "free1.html", {})
+def cars(request, *args, **kwargs):
+    return render(request, "cars.html", {})
 
-def free2(request, *args, **kwargs):
-    return render(request, "free2.html", {})
+def contact_form(request, *args, **kwargs):
+    return render(request, "contact_form.html", {})
 
-def free3(request, *args, **kwargs):
-    return render(request, "free3.html", {})
+def login(request, *args, **kwargs):
+    return render(request, "Login.html", {})
 
 def free4(request, *args, **kwargs):
     return render(request, "free4.html", {})
